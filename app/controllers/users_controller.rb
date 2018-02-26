a9b0c748261c7d4bca4535b94443ad96d941ff61
Hen Brand <hencbrand@gmail.com>
2018-02-26 17:06:27 +0000

    user controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,39 +1,7 @@
-class UserController < ApplicationController
+class UsersController < ApplicationController
 
   def show
     @user = User.find(params[:id])
-  end
-
-  def new
-    @user = User.new
-  end
-
-  def create
-    @user = User.create(user_params)
-    if @user.save
-      redirect_to user_path(@user)
-    else
-      render :new
-    end
-  end
-
-  def edit
-    @user = User.find(params[:id])
-  end
-
-  def update
-    @user = User.find(params[:id])
-    if @user.update(user_params)
-      redirect_to user_session_path(@user)
-    else
-      render :new
-    end
-  end
-
-  private
-
-  def user_params
-    params.require(:user).permit(:first_name, :last_name, :email, :password, :about_me, :profile_picture)
   end
 
 end
