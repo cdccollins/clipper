5f591459805bc64d36311634e2d025848c3e2667
Ben Fanning fanninggg@gmail.com <fanninggg@gmail.com>
2018-02-28 12:38:44 +0000

    Added some tags

--- db/seeds.rb
+++ db/seeds.rb
@@ -29,7 +29,7 @@ video = Video.new(
   title: "Test title",
   cloudinary_url: "https://res.cloudinary.com/dxtdzybai/video/upload/v1519733347/chat.mp4",
   user: user
-  tags: "cats"
+  tags: "cats nature documentary"
   )
 
 video.save!
