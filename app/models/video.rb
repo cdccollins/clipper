6e3ce765a1b1d827c5634048b235ff35c7eaffe0
Celia Collins <celia.collins@hotmail.com>
2018-03-01 10:47:25 +0000

    basic search function working

--- app/models/video.rb
+++ app/models/video.rb
@@ -2,4 +2,12 @@ class Video < ApplicationRecord
   belongs_to :user
   has_many :transactions
   mount_uploader :video, VideoUploader
+
+  include PgSearch
+
+  pg_search_scope :search_by_title_and_description,
+    against: [ :title, :description, :tags, :location ],
+    using: {
+      tsearch: { prefix: true }
+    }
 end
