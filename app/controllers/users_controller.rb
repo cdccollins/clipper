146b56b69b3512adcfeb4b85a27753911a5d5cef
Celia Collins <celia.collins@hotmail.com>
2018-03-02 12:43:18 +0000

    purchased videos now works

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,6 +2,7 @@ class UsersController < ApplicationController
 
   def show
     @videos = Video.all
+    @transactions = current_user.transactions
     @user = User.find(params[:id])
     authorize @user
   end
