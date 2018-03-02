096d0248a0b0231cd22e447c1f58bf89606ae62b
Angele Lenglemetz <angele.lenglemetz@gmail.com>
2018-03-02 14:12:27 +0000

    nothing

--- app/javascript/packs/banner.js
+++ app/javascript/packs/banner.js
@@ -6,17 +6,16 @@ function bindSweetAlertButtonDemo() {
       title: "Payment successful",
       text: "You are now the proud owner of a great video !",
       icon: "success",
-      timer:3000,
-      showConfirmationButton: false
+      timer:10000
+      // showConfirmationButton: false
     })
     event.preventDefault();
-// Event.preventDefault();
-    // document.querySelector('.swal-button').addEventListener('click', () => {
-    //   window.location.reload();
 
 
   });
-
+  // document.querySelector('.swal-button').addEventListener('click', () => {
+  // window.location.reload();
+  //   event.preventDefault();
 }
 
 
